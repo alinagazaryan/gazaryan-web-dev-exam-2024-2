cede754542f987406335b15532b086ce5cd8d495
alinagazaryan <alona25@list.ru>
2024-06-06 12:33:44 +0300

    first step remark

--- app/config.py
+++ app/config.py
@@ -2,7 +2,7 @@ import os
 
 SECRET_KEY = 'c21b53cf1799629b3ac0acd3a46581c5afd4b370127f7d2ee3face937142'
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///C:\\Users\\Huawei\\Desktop\\web_exam\\gazaryan-web-dev-exam-2024-2\\app\\database.sqlite3'
+SQLALCHEMY_DATABASE_URI = 'sqlite:///C:\\Users\\Huawei\\Desktop\\web_ex\\gazaryan-web-dev-exam-2024-2\\app\\database.sqlite3'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SQLALCHEMY_ECHO = True
 
